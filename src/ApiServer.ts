a8f5b8de2566cd45c9dd49a887ba207164b2d9f3
Spencer Clark <spencer@routerlimits.com>
2019-09-04 13:15:54 -0600

    Remove base HTTP controller

--- src/ApiServer.ts
+++ src/ApiServer.ts
@@ -2,7 +2,6 @@ import bodyParser from "body-parser";
 import express from "express";
 import http from "http";
 
-import * as baseController from "./controllers/http/base";
 import * as rlWebhookController from "./controllers/http/routerlimitswebhooks";
 import * as billingWebhookController from "./controllers/http/billingwebhooks";
 import {Configuration} from "Config";
@@ -21,8 +20,7 @@ export class ApiServer {
         this.expressApp = express();
 
         const greedyRawParser = bodyParser.raw({inflate: true, type: '*/*'});
-
-        this.expressApp.get('/', baseController.index);
+        
         this.expressApp.post('/webhooks/routerlimits', greedyRawParser, new rlWebhookController.RouterLimitsWebhookController(config, rlProcessor).router);
         this.expressApp.post('/webhooks/billing', greedyRawParser, new billingWebhookController.StripeWebhookController(config, billingProcessor).router);
         this.server = this.expressApp.listen(config.api.listenPort);
--- src/controllers/http/base.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export const index = (req: Request, res: Response) => {
-    res.send(`You've arrived at the right place, ${req.ip}`);
-};

53e4bc9114375909caa877c063ea806da710f264
Spencer Clark <spencer@routerlimits.com>
2019-09-10 18:09:05 -0600

    Fix buildApi.sh issue

--- util/buildApi.sh
+++ util/buildApi.sh
@@ -4,9 +4,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 API_DEST="$DIR/../src/routerlimits/api"
 YAML_FILE="$DIR/rlapi.yaml"
 
-LAST_GENERATION_TIME=$(date -r "$API_DEST" --rfc-3339=ns)
-touch -d "$LAST_GENERATION_TIME" /tmp/lastgentime
-NEEDS_UPDATE=$(find "$YAML_FILE" -newer /tmp/lastgentime | wc -l)
+if [ ! -d "$API_DEST" ]; then
+  NEEDS_UPDATE=1
+else
+  LAST_GENERATION_TIME=$(date -r "$API_DEST" --rfc-3339=ns)
+  touch -d "$LAST_GENERATION_TIME" /tmp/lastgentime
+  NEEDS_UPDATE=$(find "$YAML_FILE" -newer /tmp/lastgentime | wc -l)
+fi
 
 if [ "$NEEDS_UPDATE" -eq 1 ] ; then
   rm -fr "$API_DEST"
@@ -14,12 +18,3 @@ if [ "$NEEDS_UPDATE" -eq 1 ] ; then
   java -jar "$DIR/openapi-generator-cli.jar" generate -i "$YAML_FILE" -g typescript-node -o "$DIR/output"
   mv "$DIR/output" "$API_DEST"
 fi
-
-#rm -fr "$DIR/../src/routerlimits/webhooks"
-#rm -fr "$DIR/webhooks"
-#java -jar "$DIR/openapi-generator-cli.jar" generate -i "$DIR/webhooks.yaml" -g typescript-node -o "$DIR/webhooks"
-#mkdir -p "$DIR/../src/routerlimits/webhooks"
-#mv $DIR/webhooks/model/* "$DIR/../src/routerlimits/webhooks"
-#rm -fr $DIR/webhooks
-
-#sed -i "s|import { AccountsListResponse.*$|import { AccountsListResponse} from './accountsListResponse';\nimport {WebhookAccountDeleted} from './webhookAccountDeleted';\nimport {WebhookAccountSubscribed} from './webhookAccountSubscribed';|g" $DIR/../src/routerlimits/webhooks/webhook.ts
